9752b38487d3e5d8d5fa46653b12334945686a3c
potsables <postables@rtradetechnologies.com>
2019-02-02 21:43:07 -0800

    linux: add error handling

--- linux/mounter.sh
+++ linux/mounter.sh
@@ -9,18 +9,38 @@ case "$1" in
         read -r PARTITION
         echo "enter name for partition"
         read -r NAME
-        # open
+        echo "[INFO] opening drive"
         sudo cryptsetup luksOpen "$PARTITION" "$NAME"
+        if [[ "$?" -ne 0 ]]; then
+            echo "[ERROR] failed to open luks encrypted drive"
+            exit 1
+        fi
         echo "enter path to mount drive to"
         read -r MOUNT_POINT
+        echo "[INFO] mounting drive"
         sudo mount "/dev/mapper/$NAME" "$MOUNT_POINT"
+        if [[ "$?" -ne 0 ]]; then
+            echo "[ERROR] failed to mount drive"
+            exit 1
+        fi
+        echo "[INFO] successfully mounted drive"
         ;;
     close)
         echo "enter encrpted drive to unmount, ie /dev/mapper/ipfs-data"
         read -r MOUNT_POINT
+        echo "[INFO] unmounting drive"
         sudo umount /dev/mapper/ipfs-data
-        # securely close
+        if [[ "$?" -ne 0 ]]; then
+            echo "[ERROR] failed to unmount drive"
+            exit 1
+        fi
+        echo "[INFO] closing luks drive"
         sudo cryptsetup luksClose "$MOUNT_POINT"
+        if [[ "$?" -ne 0 ]]; then
+            echo "[ERROR] failed to close luks drive"
+            exit 1
+        fi
+        echo "[INFO] successfully unmounted and closed luks drive"
         ;;
     *)
         echo "invalid invocation"

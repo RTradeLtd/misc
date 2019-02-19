8feb5973d380aab9991ab222c29e9d8611daa060
potsables <postables@rtradetechnologies.com>
2019-02-19 14:08:14 -0800

    linux: slightly update ssh harden script

--- linux/ssh_harden.sh
+++ linux/ssh_harden.sh
@@ -2,12 +2,27 @@
 
 # used to harden default sshd configurations
 
-sed -i 's/AllowTcpForwarding Yes/AllowTcpForwarding No/g' /etc/ssh/sshd_config
-sed -i 's/ClientAliveCount 3/ClientAliveCount 2/g' /etc/ssh/sshd_config
-sed -i 's/Compression Yes/Compression No/g' /etc/ssh/sshd_config
-sed -i 's/LogLevel Info/LogLevel Verbose/g' /etc/ssh/sshd_config
-sed -i 's/MaxAuthTries 6/MaxAuthTries 2/g' /etc/ssh/sshd_config
-sed -i 's/MaxSessions 10/MaxSessions 2/g' /etc/ssh/sshd_config
-sed -i 's/TCPKeepAlive Yes/TCPKeepAlive No/g' /etc/ssh/sshd_config
-sed -i 's/X11Forwarding Yes/X11Forwarding No/g' /etc/ssh/sshd_config
-sed -i 's/AllowAgentForwarding Yes/AllowAgentForwarding No/g' /etc/ssh/sshd_config
+
+# remove comments and change settings
+sudo sed -i 's/AllowTcpForwarding yes/AllowTcpForwarding no/g' /etc/ssh/sshd_config
+sudo sed -i 's/ClientAliveCountMax 3/ClientAliveCountMax 2/g' /etc/ssh/sshd_config
+sudo sed -i 's/Compression yes/Compression no/g' /etc/ssh/sshd_config
+sudo sed -i 's/LogLevel INFO/LogLevel VERBOSE/g' /etc/ssh/sshd_config
+sudo sed -i 's/MaxAuthTries 6/MaxAuthTries 2/g' /etc/ssh/sshd_config
+sudo sed -i 's/MaxSessions 10/MaxSessions 2/g' /etc/ssh/sshd_config
+sudo sed -i 's/TCPKeepAlive yes/TCPKeepAlive no/g' /etc/ssh/sshd_config
+sudo sed -i 's/X11Forwarding yes/X11Forwarding no/g' /etc/ssh/sshd_config
+sudo sed -i 's/AllowAgentForwarding yes/AllowAgentForwarding no/g' /etc/ssh/sshd_config
+
+# remove comments
+sudo sed -i 's/#AllowTcpForwarding/AllowTcpForwarding/g' /etc/ssh/sshd_config
+sudo sed -i 's/#ClientAliveCountMax/ClientAliveCountMax/g' /etc/ssh/sshd_config
+sudo sed -i 's/#Compression/Compression/g' /etc/ssh/sshd_config
+sudo sed -i 's/#LogLevel/LogLevel/g' /etc/ssh/sshd_config
+sudo sed -i 's/#MaxAuthTries/MaxAuthTries/g' /etc/ssh/sshd_config
+sudo sed -i 's/#MaxSessions/MaxSessions/g' /etc/ssh/sshd_config
+sudo sed -i 's/#TCPKeepAlive/TCPKeepAlive/g' /etc/ssh/sshd_config
+sudo sed -i 's/#X11Forwarding/X11Forwarding/g' /etc/ssh/sshd_config
+sudo sed -i 's/#AllowAgentForwarding/AllowAgentForwarding/g' /etc/ssh/sshd_config
+
+echo "[INFO] please manually change password authentication, pubkey authentication, and permit root login"
